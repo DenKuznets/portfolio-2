9502f2739f5321c48406865be667dfcb2724b01e
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-09 16:35:47 +0300

    first test passes

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -5,6 +5,12 @@ export interface NavBarProps {
   onClick?: () => void;
 }
 
+export const NAVBAR_TESTIDS = {
+  NAVBAR_CONTAINER: "navbar-container",
+  NAVBAR_LIST: "navbar-list",
+  NAVBAR_LIST_ITEM: "navbar-list-item",
+};
+
 const NavBar: FC<NavBarProps> = ({ onClick }) => {
   const listElements = [];
 
@@ -19,7 +25,7 @@ const NavBar: FC<NavBarProps> = ({ onClick }) => {
   }
 
   return (
-    <nav>
+    <nav data-testid={NAVBAR_TESTIDS.NAVBAR_CONTAINER}>
       <ul className="flex items-center justify-center gap-y-10 text-center font-serif max-md:min-h-screen max-md:flex-col max-md:text-[2rem] md:gap-x-16">
         {listElements}
       </ul>
--- components/NavBar/Navbar.test.tsx
+++ components/NavBar/Navbar.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import NavBar from "./NavBar";
+import NavBar, { NAVBAR_TESTIDS } from "./NavBar";
+import "@testing-library/jest-dom";
 
 test("renders correctly", () => {
   render(<NavBar />);
+  const container = screen.getByTestId(NAVBAR_TESTIDS.NAVBAR_CONTAINER);
+  expect(container).toBeInTheDocument();
+  // const listItems = screen.getByTestId('')
 });
