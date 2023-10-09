8f847db77233d34af44e538a114fe9838ccfac67
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-09 14:42:25 +0300

    navbar mobile css done

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,27 +2,29 @@ import React, { FC } from "react";
 import text from "../../utils/text";
 
 export interface NavBarProps {
-    onClick?: () => void;
+  onClick?: () => void;
 }
 
 const NavBar: FC<NavBarProps> = ({ onClick }) => {
-    const listElements = [];
+  const listElements = [];
 
-    for (const [key, value] of Object.entries(text.header.nav)) {
-        listElements.push(
-            <li key={key}>
-                <a onClick={onClick} href={`#${key}`}>
-                    {value}
-                </a>
-            </li>
-        );
-    }
-
-    return (
-        <nav>
-            <ul bg-blue>{listElements}</ul>
-        </nav>
+  for (const [key, value] of Object.entries(text.header.nav)) {
+    listElements.push(
+      <li key={key}>
+        <a onClick={onClick} href={`#${key}`}>
+          {value}
+        </a>
+      </li>,
     );
+  }
+
+  return (
+    <nav>
+      <ul className="flex min-h-screen flex-col items-center justify-center gap-y-10 text-center text-[2rem]">
+        {listElements}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,36 +6,36 @@ const config: Config = {
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
+    fontFamily: {
+        sans: ["var(--font-raleway)"],
+        serif: ["var(--font-merriweather)", "serif"],
+        body: ["var(--font-leaguespartan)"],
+    },
     theme: {
-        fontFamily: {
-            sans: ["var(--font-raleway)"],
-            serif: ["var(--font-merriweather)", "serif"],
-            body: ["var(--font-leaguespartan)"],
-        },
-        colors: {
-            white: "#fff",
-            violet: "#6344C6",
-            darkViolet: "#262439",
-            yellow: "#FFC448",
-            almostBlack: "#11141D",
-            orange: "#F26440",
-            pTextColor: "grey",
-            aTextColor: "grey",
-        },
-        screens: {
-            uhd: "2560px",
-            laptopL: "1400px",
-            laptop: "1024px",
-            tablet: "768px",
-            mobileL: "425px",
-            mobileM: "375px",
-            mobileS: "320px",
-        },
         extend: {
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+
+            colors: {
+                violet: "#6344C6",
+                darkViolet: "#262439",
+                yellow: "#FFC448",
+                almostBlack: "#11141D",
+                orange: "#F26440",
+                pTextColor: "grey",
+                aTextColor: "grey",
+            },
+            screens: {
+                uhd: "2560px",
+                laptopL: "1400px",
+                laptop: "1024px",
+                tablet: "768px",
+                mobileL: "425px",
+                mobileM: "375px",
+                mobileS: "320px",
             },
         },
     },
