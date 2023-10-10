6a57cd3dd818057fa3786f08d35e7fa5491559bd
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-10 13:39:50 +0300

    header css added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,6 +6,7 @@ import NavBar from "../NavBar/NavBar";
 
 export const header_testids = {
   header_container: "header_container",
+  header_navbar_container: "header_navbar_container",
   header_logo_image: "header_logo_image",
   header_logo_text: "header_logo_text",
 };
@@ -14,31 +15,33 @@ const Header: FC = () => {
   return (
     <div
       data-testid={header_testids.header_container}
-      className="flex justify-between p-2 md:p-4"
+      className="top-0 z-10 flex w-full max-w-7xl justify-between p-2 shadow-md md:fixed md:p-4"
     >
-      <div>
-        <a
-          className="flex items-center text-inherit hover:no-underline"
-          href="#"
-          onClick={() => smoothScroll()}
+      <a
+        className="flex items-center text-inherit hover:no-underline"
+        href="#"
+        onClick={() => smoothScroll()}
+      >
+        <Image
+          data-testid={header_testids.header_logo_image}
+          className="mr-2"
+          width={50}
+          height={50}
+          src={logoIcon}
+          alt="html5 and hammer image"
+        />
+        <div
+          data-testid={header_testids.header_logo_text}
+          className="font-bold"
         >
-          <Image
-            data-testid={header_testids.header_logo_image}
-            className="mr-2"
-            width={50}
-            height={50}
-            src={logoIcon}
-            alt="html5 and hammer image"
-          />
-          <div
-            data-testid={header_testids.header_logo_text}
-            className="font-bold"
-          >
-            ｄｅｎ . ｋｕｚｎｅｔｓ
-          </div>
-        </a>
-      </div>
-      <div className="max-md:hidden">
+          ｄｅｎ . ｋｕｚｎｅｔｓ
+        </div>
+      </a>
+
+      <div
+        data-testid={header_testids.header_navbar_container}
+        className="flex items-center max-md:hidden"
+      >
         <NavBar />
       </div>
     </div>
