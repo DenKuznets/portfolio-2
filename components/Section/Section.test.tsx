97f64969b299f24f6d2e367700a5d473870b3552
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-12 13:48:58 +0300

    section test change

--- components/Section/Section.test.tsx
+++ components/Section/Section.test.tsx
@@ -1,14 +1,28 @@
 import { render, screen } from "@testing-library/react";
-import Section, { section_testids } from "./Section";
+import Section from "./Section";
 import "@testing-library/jest-dom";
-import { navbar_testids } from "../NavBar/NavBar";
+import { getProject } from "@/utils/projects";
+import text from '../../utils/text';
+
 
 test("renders correctly", () => {
-  render(<Section projectId={1} />);
-  const text = screen.getByTestId(section_testids.section_text);
-  const image = screen.getByTestId(section_testids.section_image);
-  expect(text).toBeInTheDocument();
-  expect(image).toBeInTheDocument();
+  const project = getProject(1);
+  if (project) {
+      render(<Section projectId={project.id} />);
+      const header = screen.getByRole('heading', { level: 3 });
+      const listItems = screen.getAllByRole('listitem');
+      const gitLink = screen.getByRole('link', { name: 'Github' });
+      const demoLink = screen.getByRole('link', { name: 'Demo' });
+      const showmoreLink = screen.getByRole('link', { name: text.showmore });
+      const img = screen.getByAltText(`${project.name} project image`);
+
+      expect(header).toHaveTextContent(project?.name);
+      expect(listItems).toHaveLength(project.tech.length);
+      expect(gitLink).toHaveAttribute('href', project.github);
+      expect(demoLink).toHaveAttribute('href', project.demo);
+      expect(showmoreLink).toHaveAttribute('href', `/allworks/${project.id}`);
+      expect(img).toBeInTheDocument();
+  }
 });
 
 test("renders Section unchanged", () => {
--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,5 +1,5 @@
 import React, { MutableRefObject, useRef } from 'react';
-import { projects, getProject } from '../../utils/projects';
+import { getProject } from '../../utils/projects';
 import Link from 'next/link';
 import MyLink from '../MyLink/MyLink';
 import text from '../../utils/text';
@@ -9,16 +9,11 @@ export interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
     projectId: number;
 }
 
-export const section_testids = {
-    section_text: 'section_text',
-    section_image: 'section_image'
-};
-
 const Section = ({ projectId }: SectionProps) => {
     const project = getProject(projectId);
     return project ? (
         <div className="container lg:flex lg:gap-8 justify-between">
-            <div data-testid={section_testids.section_text}>
+            <div>
                 <h3 className="font-bold leading-normal mb-4">
                     {project.name}
                 </h3>
@@ -40,7 +35,6 @@ const Section = ({ projectId }: SectionProps) => {
             </div>
 
             <Link
-                data-testid={section_testids.section_image}
                 className="relative hidden md:block mt-8 shadow-2xl overflow-hidden lg:mt-0"
                 href={`/allworks/${projectId}`}
             >
