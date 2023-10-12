7a2513ea3d12eaf38eb2c466a086a5efb324274a
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-12 16:23:56 +0300

    header testids removed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,13 +3,6 @@ import logoIcon from '../../public/images/logo.png';
 import NavBar from '../NavBar/NavBar';
 import Link from 'next/link';
 
-export const header_testids = {
-    header_container: 'header_container',
-    header_navbar_container: 'header_navbar_container',
-    header_logo_image: 'header_logo_image',
-    header_logo_text: 'header_logo_text'
-};
-
 const Header = () => {
     return (
         <header className="top-0 z-10 flex w-full max-w-7xl justify-between p-2 shadow-md md:fixed md:p-4">
@@ -18,7 +11,6 @@ const Header = () => {
                 href="#"
             >
                 <Image
-                    data-testid={header_testids.header_logo_image}
                     className="mr-2"
                     width={50}
                     height={50}
