e5e22149f8761e978ca246233d4a04c394232544
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-12 15:24:51 +0300

    burget refactoring

--- components/Burger/Burger.tsx
+++ components/Burger/Burger.tsx
@@ -1,19 +1,24 @@
-import Image from "next/image";
-import React, { FC } from "react";
-import hamIcon from "../../public/images/burger-menu-svgrepo-com.svg";
+import Image from 'next/image';
+import React from 'react';
+import hamIcon from '../../public/images/burger-menu-svgrepo-com.svg';
 
-export interface BurgerProps {
-  onClick: () => void;
+export interface BurgerProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    handleClick: () => void;
 }
 
-const Burger: FC<BurgerProps> = ({ onClick }) => {
-  return (
-    <div>
-      <button className="z- relative md:hidden" onClick={onClick}>
-        <Image width={30} height={30} src={hamIcon} alt="menu icon" />
-      </button>
-    </div>
-  );
+const Burger = ({ handleClick }: BurgerProps) => {
+    return (
+        <button onClick={handleClick}>
+            <Image
+                className="relative md:hidden"
+                width={30}
+                height={30}
+                src={hamIcon}
+                alt="menu icon"
+            />
+        </button>
+    );
 };
 
 export default Burger;
