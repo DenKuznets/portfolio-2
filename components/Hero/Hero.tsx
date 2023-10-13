8e0fafbfdeaa275d739a46fdb20a4343e39a26dc
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-13 12:03:56 +0300

    page anchor ids added

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -19,6 +19,7 @@ const Hero = () => {
     return (
         <div
             data-testid={hero_testids.hero_container}
+            id='#hero'
             className="container md:mt-[90px] mx-auto flex flex-col items-center lg:grid lg:grid-cols-2 lg:justify-between lg:gap-8"
         >
             <div>
