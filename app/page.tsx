dd3debd335579757dad7267ef08a783e6237136b
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-13 08:30:05 +0300

    h2 examples added

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
             <Header />
             <main className="pt-8 flex flex-col">
                 <Hero/>
+                <h2 className='text-3xl font-bold text-center my-14 bg-violet text-white py-12'>{text.examples}</h2>
                 {/* 
-                <h2 className='text-3xl font-bold text-center my-14'>{text.header.nav.work}</h2>
                 <Section className='bg-violet text-white' projectId={1} />
                 <Section projectId={2} />
                 <Section projectId={3} /> */}
