2ef3fbafeee6fac620d88bcb02446d2511065ffc
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-18 11:15:06 +0300

    fonts paddings change

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard = ({ className, projectId }: ProjectCardProps) => {
 
     return project ? (
         <Link
-            className={`container [&>img]:hover:saturate-200 shadow border-2 border-zinc-900 rounded block max-w-sm min-h-[30rem] relative ${
+            className={`container [&>img]:hover:saturate-200 shadow border-2 border-zinc-900 rounded block max-w-sm min-h-[20rem] relative ${
                 className ? className : ''
             }`}
             href={`${text.links.project}${projectId}`}
@@ -25,13 +25,13 @@ const ProjectCard = ({ className, projectId }: ProjectCardProps) => {
                 src={`/images/works-preview/${project.img}`}
                 alt={`${project.name} project image`}
             />
-            <h4 className="first-letter:capitalize shadow-xl font-bold text-white p-4 absolute top-0 left-0 bg-zinc-900/[.75] w-full">
+            <h4 className="first-letter:capitalize shadow-xl font-bold text-white p-2 absolute top-0 left-0 bg-zinc-900/[.75] w-full">
                 {project.name}
             </h4>
-            <ul className="absolute bottom-0 left-0 flex px-4 flex-wrap gap-3 items-center mb-4">
+            <ul className="absolute bottom-0 left-0 flex px-4 flex-wrap gap-2 items-center mb-4">
                 {project.tech.map((obj) => (
                     <li
-                        className="whitespace-nowrap px-4 py-2 border-dotted rounded-md border-white text-white bg-zinc-900/[.75] border"
+                        className="whitespace-nowrap text-xs px-2 py-1 border-dotted rounded-md border-white text-white bg-zinc-900/[.75] border"
                         key={obj}
                     >
                         {obj}
